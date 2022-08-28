557c16114be9c878264295e930911137b9f4d94e
manucoffin <manuel.coffin@gmail.com>
2022-08-28 09:35:02 +0200

    added PWA support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ["page.tsx", "page.ts", "page.jsx"],
 };
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  nextConfig,
+});
+// module.exports = nextConfig;
